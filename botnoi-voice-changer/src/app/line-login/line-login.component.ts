dc1f79ddc98a1246387560b4cf2c278891b3d9be
Pitchaaaaaaaa <pitcha.weesommai@gmail.com>
2022-10-15 20:42:26 +0700

    Create Line Login Component

---
+++ botnoi-voice-changer/src/app/line-login/line-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-login',
+  templateUrl: './line-login.component.html',
+  styleUrls: ['./line-login.component.css']
+})
+export class LineLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
